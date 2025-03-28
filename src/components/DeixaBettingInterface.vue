7d48879671f2bcd357507b2432b7545ed4acaed8
AaronAlejandrou <165164022+AaronAlejandrou@users.noreply.github.com>
2025-03-28 17:52:09 -0500

    update dailyImages

--- src/components/DeixaBettingInterface.vue
+++ src/components/DeixaBettingInterface.vue
@@ -196,6 +196,7 @@ const dailyImages = {
   //"2025-03-11": "public/images/bet-image3.png",
   // "2025-03-12": "/images/bet-image4.jpg",
   // "2025-03-13": "/images/bet-image5.jpg",
+  "2025-03-13": "/images/bet-image0.png",
   // Add more dates and image paths as needed
   // Format: "YYYY-MM-DD": "/path/to/your/image.jpg",
 };
